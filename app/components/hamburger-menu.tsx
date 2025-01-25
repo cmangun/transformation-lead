7fe97143149e10b9c4312cc28a8a6753073d943f
cmangun <141188820+cmangun@users.noreply.github.com>
2025-01-25 10:53:08 -0500

    Update hamburger-menu.tsx

--- app/components/hamburger-menu.tsx
+++ app/components/hamburger-menu.tsx
@@ -17,7 +17,7 @@ export default function HamburgerMenu() {
     <>
       <button
         onClick={() => setIsOpen(true)}
-        className="px-4 py-2 bg-white text-[#5B9EA6] rounded-xl shadow-lg flex items-center space-x-2"
+        className="px-8 py-2 bg-white text-[#5B9EA6] rounded-xl shadow-lg flex items-center space-x-2"
         aria-label="Open menu"
       >
         <Menu size={24} />
